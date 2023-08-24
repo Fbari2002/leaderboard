a1cde535a842e886854d847b61375e3257e33040
Fatima Bari <fatimabari2002@hotmail.com>
2023-08-24 12:42:03 +0100

    trying it again

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import About from "./About";
+ReactDOM.render(
+    <React.StrictMode>
+        <HashRouter>
+            <Routes>
+                <Route path="/" element={<App/>} />
+                <Route path="/about" element={<About/>}/>
+            </Routes>
+        </HashRouter>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
