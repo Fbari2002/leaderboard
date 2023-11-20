738c8318ba2c763456dc7eb21aac7d33f776b857
Fatima Bari <fatimabari2002@hotmail.com>
2023-11-20 20:54:46 +0000

    private and public routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import DefaultStack from './DefaultStack';
 import AuthenticatedStack from './AuthenticatedStack';
 import SignUp from './SignUpPage';
+import {useAuthentication} from "./UseAuth";
+import ProtectedRoute from "./ProtectedRoute";
 
 function App() {
-    // TODO figure out firebase auth hook that determines default/authenticated
+    const {user} = useAuthentication();
+
     return (
         <Router>
             <Routes>
                 {/* Public routes */}
-                <Route path="/leaderboard" element={<DefaultStack />} />
-                <Route path="/signup" element={<SignUp />} />
+                <Route path="/leaderboard" element={<DefaultStack/>}/>
+                <Route path="/signup" element={<SignUp/>}/>
 
                 {/* Private routes */}
-                <Route path="/dashboard" element={<AuthenticatedStack />} />
+                <Route path="/dashboard" element={
+                    <ProtectedRoute user={user}>
+                        <AuthenticatedStack/>
+                    </ProtectedRoute>
+                }/>
             </Routes>
         </Router>
     );
