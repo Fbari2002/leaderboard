8b76a096f9b30a77ecb11b88bc973210d49e2d10
Fatima Bari <fatimabari2002@hotmail.com>
2023-11-21 15:00:22 +0000

    bug fixes - hash route

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// App.js
 import React from 'react';
 import { BrowserRouter as Router, HashRouter, Routes, Route } from 'react-router-dom';
 import DefaultStack from './DefaultStack';
@@ -15,16 +16,19 @@ function App() {
     // Use BrowserRouter locally and HashRouter on GitHub Pages
     const RouterComponent = isGitHubPages ? HashRouter : Router;
 
+    // Set the basename for GitHub Pages
+    const basename = isGitHubPages ? '/leaderboard' : '/';
+
     return (
-        <RouterComponent>
+        <RouterComponent basename={basename}>
             <Routes>
                 {/* Public routes */}
-                <Route path="/leaderboard" element={<DefaultStack />} />
-                <Route path="/leaderboard/signup" element={<SignUp />} />
+                <Route path="leaderboard" element={<DefaultStack />} />
+                <Route path="leaderboard/signup" element={<SignUp />} />
 
                 {/* Private routes */}
                 <Route
-                    path="/leaderboard/dashboard"
+                    path="leaderboard/dashboard"
                     element={
                         <ProtectedRoute user={user}>
                             <AuthenticatedStack />
