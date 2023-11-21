755986a52aa0aa020aa29008053341d6baa6649b
Fatima Bari <fatimabari2002@hotmail.com>
2023-11-21 14:53:59 +0000

    bug fixes - hash route

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
 import React from 'react';
-import {HashRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, HashRouter, Routes, Route } from 'react-router-dom';
 import DefaultStack from './DefaultStack';
 import AuthenticatedStack from './AuthenticatedStack';
 import SignUp from './SignUpPage';
-import {useAuthentication} from "./UseAuth";
-import ProtectedRoute from "./ProtectedRoute";
+import { useAuthentication } from './UseAuth';
+import ProtectedRoute from './ProtectedRoute';
 
 function App() {
-    const {user} = useAuthentication();
+    const { user } = useAuthentication();
+
+    // Check if the app is deployed to GitHub Pages
+    const isGitHubPages = process.env.PUBLIC_URL.includes('github.io');
+
+    // Use BrowserRouter locally and HashRouter on GitHub Pages
+    const RouterComponent = isGitHubPages ? HashRouter : Router;
 
     return (
-        <Router>
+        <RouterComponent>
             <Routes>
                 {/* Public routes */}
-                <Route path="/leaderboard" element={<DefaultStack/>}/>
-                <Route path="/leaderboard/signup" element={<SignUp/>}/>
+                <Route path="/leaderboard" element={<DefaultStack />} />
+                <Route path="/leaderboard/signup" element={<SignUp />} />
 
                 {/* Private routes */}
-                <Route path="/leaderboard/dashboard" element={
-                    <ProtectedRoute user={user}>
-                        <AuthenticatedStack/>
-                    </ProtectedRoute>
-                }/>
+                <Route
+                    path="/leaderboard/dashboard"
+                    element={
+                        <ProtectedRoute user={user}>
+                            <AuthenticatedStack />
+                        </ProtectedRoute>
+                    }
+                />
             </Routes>
-        </Router>
+        </RouterComponent>
     );
 }
 
