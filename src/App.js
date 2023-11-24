6a74cb23dfe8777984e55adea1d2ad55c50f88e5
Fatima Bari <fatimabari2002@hotmail.com>
2023-11-24 16:21:40 +0000

    bug fixes - hash route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {HashRouter as Router, Routes, Route} from 'react-router-dom';
 import DefaultStack from './DefaultStack';
 import AuthenticatedStack from './AuthenticatedStack';
 import SignUp from './SignUpPage';
